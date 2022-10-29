5096ac9ab9c93ddee3acddc987c4ee5278c1b3b1
shokonakayama <shokochi0208@gmail.com>
2022-10-29 16:49:56 +0900

    header更新途中

---
+++ server/common/_header.php
@@ -0,0 +1,17 @@
+<header class="header wrapper">
+    <h1>
+        <a class="header_left" href="/server/users/index.php">
+            旅ログ<i class="fa-regular fa-plane"></i>
+        </a>
+    </h1>
+    <div class="header_right">
+        <li class="header_nav_item"><a href="">観光地検索</a></li>
+        <li class="header_nav_item"><a href="">旅行歴記録</a></li>
+        <li class="header_nav_item"><a href="">体験談投稿</a></li>
+        <li class="header_nav_item"><a href="">マイページ</a></li>
+        <li class="header_nav_item"><a href="">ログアウト</a></li>
+    </div>
+    <div class="header_right_side">
+        <li class="header_nav_item"><a href="">ログイン中<br>[user]さん</a></li>
+    </div>
+</header>
---
+++ server/users/index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="ja">
+        <?php include_once __DIR__ . '/../common/_head.html' ?>
+    <body>
+        <?php include_once __DIR__ . '/../common/_header.php' ?>
+    </body>
+</html>
