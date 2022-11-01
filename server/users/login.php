c0cd52401f5dd8be898612ae4fd3a59151dd0734
shokonakayama <shokochi0208@gmail.com>
2022-11-01 17:23:59 +0900

    新規登録画面のHTMLを更新

---
+++ server/users/login.php
@@ -0,0 +1,33 @@
+<?php 
+// 関数ファイルを読み込む
+require_once __DIR__ . '/../common/functions.php';
+
+$email = '';
+$password = '';
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = filter_input(INPUT_POST, 'email');
+    $password = filter_input(INPUT_POST, 'password');
+
+    $errors = login_validate($email, $password);
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<?php include_once __DIR__ . '/../common/_head.html' ?>
+<body>
+    <header class="header wrapper">
+        <h1>
+            <div class="header_left">
+                旅ログ
+            </div>
+            <i class="fa-solid fa-plane"></i>
+        </h1>
+    </header>
+    <main class="main_content content_center wrapper"></main>
+    <footer>
+        <?php include_once __DIR__ . '/../common/_footer.html' ?>
+    </footer>
+</body>
+</html>
