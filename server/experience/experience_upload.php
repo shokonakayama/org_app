009e53d0afa3e54592e12a68305defa8178473dc
kazu-taka <k.takahashi@nowall.co.jp>
2022-11-18 06:54:48 +0900

    市町村検索

--- server/experience/experience_upload.php
+++ server/experience/experience_upload.php
@@ -4,13 +4,16 @@ require_once __DIR__ . '/../common/functions.php';
 session_start();
 
 $current_user = '';
+$prefecture = '';
 
 if (isset($_SESSION['current_user'])) {
     $current_user = $_SESSION['current_user'];
 }
 
-$prefectures = find_prefecture_name();
-$addresses = find_city_name();
+$prefecture_id = filter_input(INPUT_GET, 'prefecture_id');
+$addresses = find_addresses($prefecture_id);
+$prefectures = find_prefectures();
+
 
 ?>
 
@@ -25,26 +28,23 @@ $addresses = find_city_name();
             <h2>体験談を投稿する✈︎✈︎</h2>
         </div>
         <div class="content_left">
-            <form action="mypage.php tourist_information.php" method="post">
-                <select name="prefectures">
+            <form action="" method="get">
+                <select name="prefecture_id">
                     <option value="都道府県選択">都道府県を選択してください</option>
-                    <?php foreach ($prefectures as $prefecture) : ?>
-                        <option value="<?= $prefecture['id'] ?>"><?= $prefecture['prefecture_name'] ?></option>
-                    <?php endforeach; ?>
+                        <?php foreach ($prefectures as $prefecture) : ?>
+                            <option value="<?= $prefecture['id'] ?>" <?php if ($prefecture_id == $prefecture['id']) echo 'selected'; ?>>
+                                <?= $prefecture['prefecture_name'] ?>
+                            </option>
+                        <?php endforeach; ?>
                 </select>
-                <select name="cities">
+                <input type="submit" value="検索">
+            </form>
+            <form action="mypage.php tourist_information.php" method="post">
+                <select name="address_id">
                     <option value="市町村選択">市町村を選択してください</option>
-                    <?php
-                    $sql = 'SELECT * FROM city_name';
-
-                    if ($addresses = $dbh->query($sql)) {
-
-                        foreach ($addresses as $address) {
-                            $addresses .= "<option value='" . $address['id'];
-                            $addresses .= "'>" . $address['city_name'] . "</option>";
-                        }
-                    }
-                    ?>
+                        <?php foreach ($addresses as $address) : ?>
+                            <option value="<?= $address['prefecture_id'] ?>"><?= $address['city_name'] ?></option>
+                        <?php endforeach; ?>
                     <form method="POST" action="'"></form>
                 </select>
             </form>
